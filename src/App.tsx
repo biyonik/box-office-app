646627b2420df60eee7e6db767607ef82cdaea3f
ahmet <ahmet.altun60@gmail.com>
2023-07-09 03:49:44 +0300

    Installed react-router-dom and initialised router infrastructure

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import React from 'react';
+import StarredPage from './pages/StarredPage';
+
 export default function App() {
   return (
-    <>
-      <h1>Box Office App</h1>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/starred" element={<StarredPage />} />
+        <Route path="*" element={<div>Not Found</div>} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
