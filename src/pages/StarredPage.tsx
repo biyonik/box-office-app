c361a2062ed59f4c1b09d21c3fbe3a6e209d0bbf
ahmet <ahmet.altun60@gmail.com>
2023-07-11 00:40:46 +0300

    Created useStarredShows custom hook and implementing Starred page and prepare service method. Fetch starred data on Starred page

--- src/pages/StarredPage.tsx
+++ src/pages/StarredPage.tsx
@@ -1,11 +1,27 @@
 import { useStarredShows } from '../hooks/useStarredShows';
+import { useQuery } from '@tanstack/react-query';
+import { ShowService } from '../services/show.service';
+import ShowGrid from '../components/Show/ShowGrid';
 
 export default function StarredPage() {
-  const [starredShows, _] = useStarredShows();
+  const [starredShowsIds] = useStarredShows();
+  const {
+    data: starredShows,
+    error,
+    isError,
+    isLoading,
+  } = useQuery({
+    queryKey: ['starred', starredShowsIds],
+    queryFn: () => ShowService.getShowsByIds(starredShowsIds).then(shows => shows.map(show => ({ show }))),
+    refetchOnWindowFocus: false,
+  });
 
   return (
     <div>
       <h3>Starred Page</h3>
+      {isLoading && <div>Loading...</div>}
+      {error && <div>Error occurred: {error}</div>}
+      {starredShows && <ShowGrid shows={starredShows} />}
     </div>
   );
 }
--- src/services/show.service.ts
+++ src/services/show.service.ts
@@ -1,4 +1,5 @@
 import { IShow } from '../models/IShow';
+
 const { VITE_API_URL } = import.meta.env;
 
 const BASE_SEARCH_URL = `${VITE_API_URL}/search/shows`;
@@ -21,6 +22,16 @@ export class ShowService {
     } catch (error) {
       console.error('Error fetching movie by id: ', error);
       return {} as IShow;
+    }
+  }
+
+  static async getShowsByIds(ids: string[]): Promise<IShow[]> {
+    try {
+      const promises = ids.map((showId: any) => ShowService.getShowById(showId));
+      return await Promise.all(promises);
+    } catch (error) {
+      console.error('Error fetching movies by ids: ', error);
+      return [];
     }
   }
 }
